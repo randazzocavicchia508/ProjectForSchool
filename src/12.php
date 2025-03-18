7be9b7bb870937b09396d501f82e7682f5513ee1
randazzocavicchia508 <randazzocavicchia508@gmail.com>
2025-03-18 07:18:53 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,12 @@
+
+<?php
+
+// Define an array of colors
+$colors = array("red", "green", "blue");
+
+// Loop through the array and print each color on a new line
+foreach($colors as $color) {
+    echo "$color <br>";
+}
+
+?>
