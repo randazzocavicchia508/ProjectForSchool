a26599afe237cbe6d349e6b42aaf92912e9ed9c4
randazzocavicchia508 <randazzocavicchia508@gmail.com>
2025-03-20 17:14:21 +0000

    Add 14.php

---
+++ src/14.php
@@ -0,0 +1,13 @@
+<?php
+// Simple PHP program to demonstrate basic operations with variables and loops.
+$number = 10; // Example variable
+
+// Addition
+echo "The sum of $number and 5 is: " . ($number + 5) . "<br>";
+
+// Multiplication
+echo "The result of $number multiplied by 3 is: " . ($number * 3) . "<br>";
+
+// Incrementing the number
+$incrementedNumber = $number + 1; // $incrementedNumber will be 10 + 1 = 11
+echo "Incrementing the number results in: " . $incrementedNumber . "<br>";
