76c59aa173f53978053d7a9ad7b26dee06f30239
randazzocavicchia508 <randazzocavicchia508@gmail.com>
2025-03-29 19:45:08 +0000

    Add 18.php

---
+++ src/18.php
@@ -0,0 +1,17 @@
+<?php
+// This is a simple PHP script to demonstrate basic HTML and PHP functionality.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Project For School - Simple Script</title>
+</head>
+<body>
+
+<h1>Welcome to Project For School!</h1>
+<p>This is a simple project for school. It's designed to help students learn and explore in an educational environment.</p>
+
+</body>
+</html>
